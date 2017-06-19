dc00d8a8839d1cb72553dead5c47114f994112b4
CharlotteLi <1172927750@qq.com>
2017-06-19 16:08:57 +0800

    react

---
+++ src/component/Header.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/6/4.
+ */
+import React from 'react';
+
+export default class Header extends React.Component{
+    render(){
+        return <h1>恶心恶心</h1>
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack=require('webpack');
+const path=require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+
+module.exports={
+    entry:path.resolve(__dirname,'./src/index.js'),
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js(x)?$/,
+                use:'babel-loader',
+                exclude:/node_modules/
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        })
+    ]
+
+};
